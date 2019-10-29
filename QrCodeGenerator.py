0d0b73ab2a90a1ae456f8975df5cd77b6f8e3002
Adesina Olusegun Abiola <shegmatech@gmail.com>
2019-10-29 15:30:12 +0100

    Update and rename MyList_FileinDir.py to QrCodeGenerator.py

--- QrCodeGenerator.py
+++ QrCodeGenerator.py
@@ -3,10 +3,7 @@ import pyqrcode
 import png #pyqrcode cant generate png images so i need to use this module to generate png.
 
 # getting document path with os
-path = 'C:\\Users\\MSE\\PycharmProjects\\qrcode_gen\\Passport'
-
-#Output folder
-output = 'C:\\Users\\MSE\\PycharmProjects\\qrcode_gen\\QR'
+path = 'C:\\Users\\MSE\\PycharmProjects\\qrcode_gen\\Passport' #change this path to where your passports are located
 
 # using os to get the file name without extention
 files = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
@@ -16,8 +13,6 @@ for line in files:
     i = str(line)
     #using pyqrcode to create the barcode
     qr = pyqrcode.create(line)
-    ##url.svg(line + '.jpg', scale=8)
-    ##print(url.terminal(quiet_zone=1))
     #Barcode output with the filename
     qr.png(line + '.png', scale=5)
     qr.png(line + '.png', scale=5, )
